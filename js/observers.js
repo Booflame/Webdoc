54b364192645aeebd492e4cb74fc2deb982824a6
Martin Elsborg <Martin.elsborg@gmail.com>
2022-04-04 09:53:41 +0200

    Tilføjet observer til video

--- js/observers.js
+++ js/observers.js
@@ -51,3 +51,18 @@ const typeWriterObs = new IntersectionObserver( entries => {
 document.querySelectorAll("[data-title]").forEach(element => {
     typeWriterObs.observe(element)
 })
+
+if(!!window.IntersectionObserver){
+    let video = document.querySelector('.rejse');
+    let isPaused = false; /* flag for auto-pausing of the video */
+    let observer = new IntersectionObserver((entries, observer) => { 
+      entries.forEach(entry => {
+        if(entry.intersectionRatio!=1  && !video.paused){
+          video.pause(); isPaused = true;
+        }
+        else if(isPaused) {video.play(); isPaused=false}
+  
+      });
+    }, {threshold: 1});
+    observer.observe(video) ;
+  }
